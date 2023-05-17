3265b740925f41fb265f5f117d83ade4f0549bdf
Himorell <himorellj@gmail.com>
2023-05-17 18:14:15 +0200

    fix methods´s RecipesControllers

--- app/Http/Controllers/Api/RecipeController.php
+++ app/Http/Controllers/Api/RecipeController.php
@@ -9,8 +9,8 @@ class RecipeController extends Controller
 {
     public function index()
     {
-        // Obtener todos los registros de la tabla 'recipes'
-        $recipes = Recipe::all();
+        // Obtener todos los registros de la tabla 'recipes' ordenados por orden alfabetico
+        $recipes = Recipe::orderBy('title')->get();
 
         return response()->json($recipes);
     }
@@ -18,56 +18,52 @@ class RecipeController extends Controller
     public function store(Request $request)
     {
         // Validar los datos de entrada
-        $request->validate([
-            'title' => 'required',
-            'imgRecipe' => 'required',
-            'description' => 'required',
-            'timeCook' => 'required',
-            'portions' => 'required',
-            'ingredients' => 'required',
-            'instructions' => 'required',
-            'user_id' => 'required|exists:users,id'
+        $data = $request->validate([
+            'title' => 'required|string|max:255',
+            'imgRecipe' => 'nullable|string',
+            'description' => 'required|string',
+            'timeCook' => 'required|string',
+            'portions' => 'required|string',
+            'ingredients' => 'required|string',
+            'instructions' => 'required|string',
+            'user_id' => 'required|integer|exists:users,id'
         ]);
 
-        // Crear un nuevo registro en la tabla 'recipes'
-        $recipe = Recipe::create($request->all());
+        // Crear una nueva receta con los datos validados
+        $recipe = Recipe::create($data);
 
         return response()->json($recipe, 201);
     }
 
-    public function show($id)
+    public function show(Recipe $recipe)
     {
-        // Obtener un registro específico de la tabla 'recipes'
-        $recipe = Recipe::findOrFail($id);
-
+        // Devolver la receta en formato JSON
         return response()->json($recipe);
     }
 
-    public function update(Request $request, $id)
+    public function update(Request $request, Recipe $recipe)
     {
         // Validar los datos de entrada
-        $request->validate([
-            'title' => 'required',
-            'imgRecipe' => 'required',
-            'description' => 'required',
-            'timeCook' => 'required',
-            'portions' => 'required',
-            'ingredients' => 'required',
-            'instructions' => 'required',
-            'user_id' => 'required|exists:users,id'
+        $data = $request->validate([
+            'title' => 'required|string|max:255',
+            'imgRecipe' => 'nullable|string',
+            'description' => 'required|string',
+            'timeCook' => 'required|string',
+            'portions' => 'required|string',
+            'ingredients' => 'required|string',
+            'instructions' => 'required|string',
         ]);
 
-        // Actualizar un registro específico en la tabla 'recipes'
-        $recipe = Recipe::findOrFail($id);
-        $recipe->update($request->all());
+        // Crear una nueva receta con los datos validados
+        $recipe = Recipe::update($data);
 
-        return response()->json($recipe);
+        return response()->json($recipe, 201);
     }
 
-    public function destroy($id)
+    public function destroy(Recipe $recipe)
     {
         // Eliminar un registro específico de la tabla 'recipes'
-        $recipe = Recipe::findOrFail($id);
+        //$recipe = Recipe::findOrFail($id);
         $recipe->delete();
 
         return response()->json(null, 204);
