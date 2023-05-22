7cd36de0cf2003c47f696789464569cfea636d22
pagudelo884 <pagudelo884@gmail.com>
2023-05-22 12:54:25 +0200

    add authController and endpoints login, register and logout

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\Api\RecipeController;
 
 
@@ -19,6 +20,21 @@ use App\Http\Controllers\Api\RecipeController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::post('myRecipes', [AuthController::class, 'myRecipes'])->name('myRecipes');
+    Route::post('register', [AuthController::class, 'register']);
+
 });
 
 Route::get('/recipes', [RecipeController::class, 'index'])->name('recipesApi');
