1d3fa9e730265eebbb6b0da685cafef5158e032a
Himorell <himorellj@gmail.com>
2023-05-29 10:21:38 +0200

    change name routes

--- routes/api.php
+++ routes/api.php
@@ -38,9 +38,9 @@ Route::group([
 });
 
 Route::get('/recipes', [RecipeController::class, 'index'])->name('recipesApi');
-Route::post('/createRecipes', [RecipeController::class, 'store'])->name('createRecipesApi')->middleware('auth');
+Route::post('/recipes', [RecipeController::class, 'store'])->name('createRecipesApi')->middleware('auth');
 Route::get('/recipes/{id}', [RecipeController::class, 'show'])->name('showRecipesApi');
-Route::put('/updateRecipes/{id}', [RecipeController::class, 'update'])->name('updateRecipesApi')->middleware('auth');
-Route::delete('/deleteRecipes/{id}', [RecipeController::class, 'destroy'])->name('deleteRecipesApi')->middleware('auth');
+Route::put('/recipes/{id}', [RecipeController::class, 'update'])->name('updateRecipesApi')->middleware('auth');
+Route::delete('/recipes/{id}', [RecipeController::class, 'destroy'])->name('deleteRecipesApi')->middleware('auth');
 Route::get('/recipes/search/{title}', [RecipeController::class, 'search']);
 Route::get('/myRecipes', [RecipeController::class, 'myRecipes'])->middleware('auth');
